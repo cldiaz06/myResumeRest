f29d61f9b780a142247ecf1785fd96dc213e9c99
cldiaz06 <cldiaz06@gmail.com>
2018-09-20 16:48:57 -0500

    Created controller. Need to fix finding json files

---
+++ src/main/java/com/cldiaz/myResumeRest/controllers/ResumeRestController.java
@@ -0,0 +1,26 @@
+package com.cldiaz.myResumeRest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cldiaz.myResumeRest.models.Resume;
+import com.cldiaz.myResumeRest.servicesImpl.JsonGetResume;
+
+@RestController
+@RequestMapping("/rest")
+public class ResumeRestController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/getResume")
+	public Resume getResume() {
+		
+		JsonGetResume getResume = new JsonGetResume();
+		Resume resume = getResume.getResume();
+		return resume;
+	}
+}
