cc9bab538a55227979121361da677941abf614fd
cldiaz06 <cldiaz06@gmail.com>
2018-09-21 10:42:15 -0500

    AddController for PDF

--- src/main/java/com/cldiaz/myResumeRest/config/WebAppConfig.java
+++ src/main/java/com/cldiaz/myResumeRest/config/WebAppConfig.java
@@ -1,12 +1,48 @@
 package com.cldiaz.myResumeRest.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.cldiaz.myResumeRest")
-public class WebAppConfig {
-
+public class WebAppConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer
+				.defaultContentType(MediaType.APPLICATION_JSON)
+				.favorPathExtension(true);
+	}
+	
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		
+		resolver.setContentNegotiationManager(manager);
+		
+		List<ViewResolver> resolvers = new ArrayList<>();
+		
+		resolvers.add(pdfViewResolver());
+		
+		resolver.setViewResolvers(resolvers);
+		return resolver;
+	}
+	
+	@Bean
+	public ViewResolver pdfViewResolver() {
+		return new PdfViewResolver();
+	}
+	
 }
