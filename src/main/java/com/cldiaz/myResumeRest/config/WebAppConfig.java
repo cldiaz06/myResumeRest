92dff00a6f602bffa6595303a398d38a2ea59782
cldiaz06 <cldiaz06@gmail.com>
2018-09-22 16:39:56 -0500

    readded config for jsp support

--- src/main/java/com/cldiaz/myResumeRest/config/WebAppConfig.java
+++ src/main/java/com/cldiaz/myResumeRest/config/WebAppConfig.java
@@ -29,35 +29,35 @@ public class WebAppConfig implements WebMvcConfigurer{
 //				.favorPathExtension(true);
 //	}
 //	
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		
+		resolver.setContentNegotiationManager(manager);
+		
+		List<ViewResolver> resolvers = new ArrayList<>();
+		
+		//resolvers.add(pdfViewResolver());
+		resolvers.add(jspViewResolver());
+		
+		resolver.setViewResolvers(resolvers);
+		return resolver;
+	}
+	
 //	@Bean
-//	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
-//		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
-//		
-//		resolver.setContentNegotiationManager(manager);
-//		
-//		List<ViewResolver> resolvers = new ArrayList<>();
-//		
-//		//resolvers.add(pdfViewResolver());
-//		resolvers.add(jspViewResolver());
-//		
-//		resolver.setViewResolvers(resolvers);
-//		return resolver;
+//	public ViewResolver pdfViewResolver() {
+//		return new ViewResolver;//PdfViewResolver();
 //	}
 //	
-////	@Bean
-////	public ViewResolver pdfViewResolver() {
-////		return new ViewResolver;//PdfViewResolver();
-////	}
-////	
-//
-//	
-//	@Bean
-//	public ViewResolver jspViewResolver() {
-//		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//		viewResolver.setViewClass(JstlView.class);
-//		viewResolver.setPrefix("/WEB-INF/views");
-//		viewResolver.setSuffix(".jsp");
-//		return viewResolver;
-//	}
+
+	
+	@Bean
+	public ViewResolver jspViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
 	
 }
