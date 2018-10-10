30bcc24364673008becffdf31494603fb4d8a2a1
cldiaz06 <cldiaz06@gmail.com>
2018-10-10 10:29:11 -0500

    got model data to default home

--- src/main/java/com/cldiaz/myResumeRest/controllers/MvcWebController.java
+++ src/main/java/com/cldiaz/myResumeRest/controllers/MvcWebController.java
@@ -1,20 +1,63 @@
 package com.cldiaz.myResumeRest.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cldiaz.myResumeRest.config.ConfigProperties;
+import com.cldiaz.myResumeRest.dataImport.JsonGetResume;
+import com.cldiaz.myResumeRest.dataImport.XmlGetResume;
+import com.cldiaz.myResumeRest.models.Resume;
 
 @Controller
 @RequestMapping("/")
 public class MvcWebController {
-
+	
+	@Autowired
+	private JsonGetResume jsonGetResume;
+	
+	@Autowired
+	private XmlGetResume xmlGetResume;
+	
+	private ConfigProperties prop;
+	private Resume res;
+	
+	private String getPropFileType() {
+		return prop.getFileType();
+	}
+	
+	private String getPropTemplate() {
+		return prop.getTemplate();
+	}
+	
+	@Autowired
+	public void setGlobalProperties(ConfigProperties prop) {
+		this.prop = prop;
+	}
+	
+	@ModelAttribute("resume")
+	public void setResume(Resume res) {
+		if ((!prop.getFileType().isEmpty()) && (prop.getFileType().equals("xml"))) {
+			this.res = xmlGetResume.getResume(false);
+		}
+		else {
+			this.res = jsonGetResume.getResume(false);
+		}
+	}
+	
 	@GetMapping("/home")
 	public String viewHome(Model model) {
 		
-		model.addAttribute("message","Welcome to Spring and Thymeleaf");
+		System.out.println(res.toString());
+		
+		model.addAttribute("basicInfo",res.getBasicInfo());
 		
 		return "index";
 	}
+	
+
 	
 }
